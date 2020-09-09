d9098d1bbdcbcea6119a229666afd378d26c0559
JerryTheQuad <42748875+JerryTheQuad@users.noreply.github.com>
2020-09-09 20:37:03 +0300

    Update 0.1
    I changed the way temperature is displayed. Now it is only average because if I put it like that (min-max), than there is a high chance the bot will put two identical numbers, although they are float.
    
    Another thing — now, instead of telling what's going on in the skies (clear sky, rain, etc.) it outputs corresponding emoji. It's still should be adjusted according to the time in the place because if there is night and the sky is clear, my bot will output the Sun emoji.

--- weather_please_bot.py
+++ weather_please_bot.py
@@ -7,25 +7,23 @@ Original file is located at
     https://colab.research.google.com/drive/1q3RhVC94xPOHati8eiUeNzFwIBYJpxxG
 """
 
-# !pip install pyowm
-# !pip install pyTelegramBotAPI==3.7.3
-
 from pyowm import OWM
+from pyowm.utils.config import get_default_config
 import telebot
 
+weather_status = {'ясно': '\U00002600', 'облачно с прояснениями': '\U0001F325', 'небольшой дождь': '\U0001F4A7', 'переменная облачность': '\U000026C5', 'пасмурно': '\U00002601'}
 
-owm = OWM()
-obs = owm.weather_at_place()
-w = obs.get_weather()
-w.get_temperature(unit='celsius')
+config_dict = get_default_config()
+config_dict['language'] = 'ru'
+owm = OWM('708c7a4d5f293058b24273505d91e26a', config_dict)
 
-bot = telegram.TeleBot()
+bot = telebot.TeleBot("1378585680:AAGf7inMDvfjsf8DC51dczc_i8l-OHcVZWw")
 
 @bot.message_handler(commands=['start', 'help'])
 def commands(message):
   if message.text == '/start':
 	  bot.send_message(message.from_user.id, """Привет! Я могу найти тебе погоду в нужном городе. Условие:
-1. Никому не рассказывай о бойцовском клубе 
+1. Никому не рассказывай о бойцовском клубе
 2. Пиши в подобном формате 'Москва, РФ' или 'Москва'
 3. Только на русском языке
 У меня есть чувство, что мы с тобой сработаемся.
@@ -33,21 +31,21 @@ def commands(message):
   elif message.text == '/help':
     bot.send_message(message.from_user.id, """Смотри. Ты поможешь мне, я помогу тебе:
 1. Я понимаю только в формате, подобном этому — 'Москва, РФ' или 'Москва'
-2. Если я не отвечаю, то здесь две причины: или меня отключили, или ты вводишь город/страну не в том формате. 
+2. Если я не отвечаю, то здесь две причины: или меня отключили, или ты вводишь город/страну не в том формате.
 Но я тебе пишу, верно? Значит с меня взятки гладки.
 """)
 
 @bot.message_handler(content_types=['text'])
 def handle_messages(message):
-  obs = owm.weather_at_place(message.text)
-  w = obs.get_weather()
-  now_temp = w.get_temperature(unit='celsius')
-  status = w.get_detailed_status()
-  bot.send_message(message.from_user.id, f'''Температура в {message.text} следующая:
-Минимальная температура: {now_temp.get("temp_min")}С
-Средняя температура: {now_temp.get("temp")}С
-Максимальная температура: {now_temp.get("temp_max")}C
-На небе: {status}
+    global weather_status
+
+    mgr = owm.weather_manager()
+    observation = mgr.weather_at_place(message.text)
+    w = observation.weather
+    now_temp = w.temperature('celsius')
+    status = w.detailed_status
+    bot.send_message(message.from_user.id, f'''Температура в {message.text} составляет в среднем {now_temp.get("temp")}С
+На небе: {weather_status.get(status)}
 ''')
 
 bot.polling()
